4488a77f5f5ebe59e47f446e8d408e04c126e73b
newwrld-dev <usermmust@gmail.com>
2026-01-05 15:06:30 +0300

    Update index.js

--- index.js
+++ index.js
@@ -268,7 +268,7 @@ conn?.ev?.on('messages.update', async updates => {
     }
   if (mek.key && mek.key.remoteJid === 'status@broadcast' && config.AUTO_STATUS_REACT === "true"){
     const ravlike = await conn.decodeJid(conn.user.id);
-    const emojis = ['❤️', '🖤', '💚', '💞'];
+    const emojis = ['❤️', '💸', '😇', '🍂', '💥', '💯', '🔥', '💫', '💎', '💗', '🤍', '🖤', '👀', '🙌', '🙆', '🚩', '🥰', '💐', '😎', '🤎', '✅', '🫀', '🧡', '😁', '😄', '🌸', '🕊️', '🌷', '⛅', '🌟', '🗿', '🇵🇰', '💜', '💙', '🌝', '🖤', '💚'];
     const randomEmoji = emojis[Math.floor(Math.random() * emojis.length)];
     await conn.sendMessage(mek.key.remoteJid, {
       react: {
